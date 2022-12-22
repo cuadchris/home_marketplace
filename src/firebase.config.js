360f9bb1ddc023dfc77c481124841a1139c71485
Christopher Cuadrado <cuadradochris@hotmail.com>
2022-12-22 09:59:13 -0500

    initial clean-up. assets brought in

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,11 @@ const firebaseConfig = {
   projectId: "home-marketplace-1122d",
   storageBucket: "home-marketplace-1122d.appspot.com",
   messagingSenderId: "1015129019073",
-  appId: "1:1015129019073:web:f4c82cbfd3868591a7ba40"
+  appId: "1:1015129019073:web:f4c82cbfd3868591a7ba40",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
