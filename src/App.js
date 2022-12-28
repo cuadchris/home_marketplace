5dd4f85e60626cd8baa33bc33e3bb6bad680b9bb
Christopher Cuadrado <cuadradochris@hotmail.com>
2022-12-28 17:55:52 -0500

    contact page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -19,10 +20,16 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Explore />} />
+
+          <Route path="/contact/:landlordId" element={<Contact />} />
+
           <Route path="/offers" element={<Offers />} />
           <Route path="/create-listing" element={<CreateListing />} />
           <Route path="/category/:categoryName" element={<Category />} />
-          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,109 +1,109 @@
-import { getAuth } from 'firebase/auth'
-import { doc, getDoc } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
-import { Link, useNavigate, useParams } from 'react-router-dom'
-import shareIcon from '../assets/svg/shareIcon.svg'
-import Spinner from '../components/Spinner'
-import { db } from '../firebase.config'
+import { getAuth } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import shareIcon from "../assets/svg/shareIcon.svg";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase.config";
 
 const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
-    const [listing, setListing] = useState(null)
-    const [loading, setLoading] = useState(true)
-    const [shareLinkCopied, setShareLinkCopied] = useState(false)
+  const navigate = useNavigate();
+  const params = useParams();
+  const auth = getAuth();
 
-    const navigate = useNavigate()
-    const params = useParams()
-    const auth = getAuth()
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
 
-    useEffect(() => {
-        const fetchListing = async () => {
-            const docRef = doc(db, "listings", params.listingId)
-            const docSnap = await getDoc(docRef)
+      if (docSnap.exists()) {
+        console.log(docSnap.data());
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
 
-            if(docSnap.exists()){
-                console.log(docSnap.data())
-                setListing(docSnap.data())
-                setLoading(false)
-            }
-        }
+    fetchListing();
+  }, [params.listingId]);
 
-        fetchListing()
-    }, [params.listingId])
-
-    if(loading){
-        return <Spinner/>
-    }
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <main>
-        {/* slider */}
+      {/* slider */}
 
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <img src={shareIcon} alt="" />
+      </div>
 
-        <div className="shareIconDiv" onClick={() => {
-            navigator.clipboard.writeText(window.location.href)
-            setShareLinkCopied(true)
-            setTimeout(() => {
-                setShareLinkCopied(false)
-            }, 2000)
-        }}>
-            <img src={shareIcon} alt="" />
-        </div>
+      {shareLinkCopied && <p className="linkCopied">Link copied!</p>}
 
+      <div className="listingDetails">
+        <p className="plistingName">
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </p>
 
-        {shareLinkCopied && <p className='linkCopied'>Link copied!</p>}
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">
+          For {listing.type === "rent" ? "rent" : "sale"}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
 
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} bedrooms`
+              : "1 bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} bathrooms`
+              : "1 bathroom"}
+          </li>
+          <li>{listing.parking && "Parking"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
+        </ul>
 
-        <div className="listingDetails">
-            <p className="plistingName">
-                {listing.name} - ${listing.offer ? listing.discountedPrice.toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : listing.regularPrice.toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-            </p>
+        <p className="listingLocationTitle">Location</p>
 
-            <p className="listingLocation">{listing.location}</p>
-            <p className="listingType">For {listing.type === "rent" ? "rent" : "sale"}</p>
-            {listing.offer && (
-                <p className='discountPrice'>
-                    ${listing.regularPrice - listing.discountedPrice} discount
-                </p>
-            )}
+        {/* future map */}
 
-
-            <ul className="listingDetailsList">
-                <li>
-                {listing.bedrooms > 1 ? `${listing.bedrooms} bedrooms` : "1 bedroom"}
-                </li>
-                <li>
-                {listing.bathrooms > 1 ? `${listing.bathrooms} bathrooms` : "1 bathroom"}
-                </li>
-                <li>
-                    {listing.parking && "Parking"}
-                </li>
-                <li>
-                    {listing.furnished && "Furnished"}
-                </li>
-            </ul>
-
-
-            <p className="listingLocationTitle">Location</p>
-
-
-            {/* future map */}
-
-
-
-            {auth.currentUser?.uid != listing.userRef && (
-                <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className="primaryButton">
-                    Contact Landlord
-                </Link>
-            )}
-
-
-        </div>
-
-
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className="primaryButton"
+          >
+            Contact Landlord
+          </Link>
+        )}
+      </div>
     </main>
-  )
-}
-export default Listing
+  );
+};
+export default Listing;
